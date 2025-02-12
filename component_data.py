3e259beee791dbb971fb8f154d701265ce4834d3
Brady Ligon <brady.ligon@dell.com>
2025-02-12 16:22:54 -0600

    Editing of text fields for networking and compute now functional

--- component_data.py
+++ component_data.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import pandas as pd
 
 # This file is used to create classes to store data for each type of component.
 # The idea is to allow us to then input this into the generate_tech_profile as 
@@ -6,14 +7,57 @@ from dataclasses import dataclass
 
 # Class definitions
 @dataclass
-class Networking:
-    switchModel: str
+class Component:
+    model: str
     qty: int
-    speed: str
-    numPorts: int
 
+@dataclass
+class Compute:
+    server: Component
+    cpu: Component
+    memory: str
+
+# @dataclass
+# class Storage:
+#     storageModel: str
+#     capacity: int
+
+# @dataclass
+# class Backup:
+#     backupModel: str
+#     backupDesc: str
+
+# @dataclass
+# class DR:
+#     drApp: str
+#     drDesc: str
+
+# @dataclass
+# class Virtualization:
+#     virtName: str
+#     virtDesc: str
 
 # Component definitions
 
-# Networking
-switch_S4128F = Networking("PowerSwitch S4128F", 2, "10 GbE SFP+", 28)
+def gen_switch_data():
+    # Networking
+    df = pd.read_csv("switch_data.csv")
+
+    switches = {}
+
+    for _, row in df.iterrows():
+        model = row['Switch Model']
+        speed = row['Speed']
+        ports = row['Num Ports']
+        switches[model] = {
+            'speed': speed,
+            'ports': ports
+        }
+
+    # Print database (for debugging)
+    # for name, specs in switches.items():
+    #     print(f"Name of Switch: {name}")
+    #     print(f"Speed of Switch: {specs['speed']}")
+    #     print(f"Number of Ports: {specs['ports']}")
+
+    return switches
--- generate_tech_profile.py
+++ generate_tech_profile.py
@@ -51,11 +51,21 @@ def load_image_data(slide, imageNames):
 # -------------------- MAIN FUNCTION -------------------------
 
 def generate_tech_profile():
+    # Generates the tech profile. Below are parameters to add in later:
+    # initTxt, challTxt, workloadTxt, virtualizationInput, computeInput, networkInput, managementInput, storageInput, replicationInput, backupInput
+    numSites = 1
+    networkInput = Component("PowerSwitch S5224F", 2)
+    managementInput = Component("PowerSwitch S4128T", 1)
+    computeInput = []
+    computeInput.append(Compute(Component("PowerEdge R660", 3), Component("Intel Gold 6526Y, 2.8 GHz, 16C/32T", 1), "512 GiB"))
 
     # Load template presentation
     tp = Presentation("template_v2.pptx")
 
-    numSites = 1
+
+    # ------------------- LOAD NECESSARY DATA ---------------------
+    # -------------------------------------------------------------
+
     singleImageNames = ["Switch1", "Switch2", "ManagementSwitch", "Server1", "Server2", "Server3", "Server4", "Server5", "Server6", "Server7", "Server8", "Server9", "Server10", "Server11", "Server12", "Storage", "Backup", "Virtualization"]
     doubleImageNames = ["Switch1_1", "Switch2_1", "Switch1_2", "Switch2_2", "ManagementSwitch1", "ManagementSwitch2", "Server1_1", "Server2_1", "Server3_1", "Server4_1", "Server5_1", "Server6_1", "Server1_2", "Server2_2", "Server3_2", "Server4_2", "Server5_2", "Server6_2", "Storage1", "Storage2", "Backup1", "Backup2", "Virtualization"]
 
@@ -65,51 +75,88 @@ def generate_tech_profile():
     if numSites == 1:
         imageData = load_image_data(slide, singleImageNames)
 
-        # Print the results
-        for name, dims in imageData.items():
-            print(f"Shape: {name}")
-            print(f"  Left: {dims['left']}")
-            print(f"  Top: {dims['top']}")
-            print(f"  Width: {dims['width']}")
-            print(f"  Height: {dims['height']}")
+        # Print the results (for debugging)
+        # for name, dims in imageData.items():
+        #     print(f"Shape: {name}")
+        #     print(f"  Left: {dims['left']}")
+        #     print(f"  Top: {dims['top']}")
+        #     print(f"  Width: {dims['width']}")
+        #     print(f"  Height: {dims['height']}")
 
     else:
         imageData = load_image_data(slide, doubleImageNames)
 
-        # Print the results
-        for name, dims in imageData.items():
-            print(f"Shape: {name}")
-            print(f"  Left: {dims['left']}")
-            print(f"  Top: {dims['top']}")
-            print(f"  Width: {dims['width']}")
-            print(f"  Height: {dims['height']}")
+        # Print the results (for debugging)
+        # for name, dims in imageData.items():
+        #     print(f"Shape: {name}")
+        #     print(f"  Left: {dims['left']}")
+        #     print(f"  Top: {dims['top']}")
+        #     print(f"  Width: {dims['width']}")
+        #     print(f"  Height: {dims['height']}")
+
+    # Load switch data
+    switches = gen_switch_data()
 
 
-
-    # Edit text fields
+    # --------------------- EDIT TEXT FIELDS ----------------------
+    # -------------------------------------------------------------
 
     for shape in slide.shapes:
         if shape.name == "Network_Info":
             # Edit text field for networking
-            # In context, the one switch component will become the user input to the function, networkInput
             paragraphs = shape.text_frame.paragraphs
 
-            # ToR Model Description
-            switchModelTxt = "(" + str(switch_S4128F.qty) + ") " + switch_S4128F.switchModel
-            update_paragraph(paragraphs[1], switchModelTxt, True)
+            for name in switches:
+                if networkInput.model == name:
+                    # ToR Model Description
+                    switchModelTxt = "(" + str(networkInput.qty) + ") " + networkInput.model
+                    update_paragraph(paragraphs[1], switchModelTxt, True)
 
-            # Speed Description
-            switchSpeedTxt = switch_S4128F.speed
-            update_paragraph(paragraphs[2], switchSpeedTxt, False)
+                    # Speed Description
+                    switchSpeedTxt = switches[name]['speed']
+                    update_paragraph(paragraphs[2], switchSpeedTxt, False)
 
-            # Num Ports Description
-            switchPortsTxt = str(switch_S4128F.numPorts) + " Ports"
-            update_paragraph(paragraphs[3], switchPortsTxt, False)
+                    # Num Ports Description
+                    switchPortsTxt = switches[name]['ports']
+                    update_paragraph(paragraphs[3], switchPortsTxt, False)
+                if managementInput.model == name:
+                    # Management Switch Description
+                    managementTxt = "(" + str(managementInput.qty) + ") " + managementInput.model
+                    update_paragraph(paragraphs[5], managementTxt, True)
 
-            # Management Switch Description
+        if shape.name == "Compute_Info":
+            # Edit text field for compute
+            paragraphs = shape.text_frame.paragraphs
+
+            row = 0
+            for compute in computeInput:
+                # Server Model Description
+                serverTxt = "(" + str(compute.server.qty) + ") " + compute.server.model  
+                update_paragraph(paragraphs[row], serverTxt, True)
+                row = row + 1
+
+                # CPU Description
+                cpuTxt = "(" + str(compute.cpu.qty) + ") " + compute.cpu.model
+                update_paragraph(paragraphs[row], cpuTxt, False)
+                row = row + 1
+
+                # Memory Description
+                memoryTxt = compute.memory
+                update_paragraph(paragraphs[row], memoryTxt, False)
+                row = row + 1
+
+            for i in range(row, 9):
+                paragraphs[i].clear()
+
+        
             
 
-            # print(paragraphs)
+
+
+    # --------------------- COPY AND PAST IMAGES ----------------------
+    # ----------------------------------------------------------------- 
+
+
 
     # Save tech profile
     fileName = "test"
